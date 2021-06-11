ecc4a9994a6e35eeb5d0235cb5344b0fcca4039a
Sebastian Matthew Leal <sebastianmatthewleal@Sebastians-MacBook-Pro.local>
2021-06-11 15:43:16 -0700

    we'll see

--- index.js
+++ index.js
@@ -343,9 +343,9 @@ The function should console.log 50 chunks of HTML code that match the structure 
 
 ‼️ You do **NOT** need to get these to display on your page, but you can copy and paste the result into your HTML file if you'd like to see what that would look like. */
 
-function getHTML(/* Code here */){
+function getHTML(data){
 
-    /* Code here */
+    
 
   }
 
@@ -353,10 +353,12 @@ function getHTML(/* Code here */){
 /* 💪💪💪💪💪💪 STRETCH 2: 💪💪💪💪💪💪
 Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
 
-function randomize(/* Code here */){
-
-    /* Code here */
-
+function randomize(array){
+const randomArray = []
+    for (let i = 0; i < array.length; i++){
+     randomArray.push.array.random()
+    }
+return randomArray;
   }
 
 
