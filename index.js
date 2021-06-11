a5cfefdacf3a91a53446ee8a1e4acd5216abbe50
Sebastian Matthew Leal <sebastianmatthewleal@Sebastians-MacBook-Pro.local>
2021-06-11 14:15:38 -0700

    almost there

--- index.js
+++ index.js
@@ -243,11 +243,15 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(array, name){
+function get20s(array){
   const lived = []
-  if(array["years"].include("19") {
-      lived.push(array['name']);
+ 
+  for(let i = 0; i < array.length; i++){
+    if(array['years'].include('19')){
+      lived.filter(['name'])
     }
+    
+  }
   return lived;
 }
 
@@ -262,10 +266,10 @@ function get20s(array, name){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(array, num){
+  array.pop(array[num]);
+  return array.length;
 }
-   
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use addArtist to do the following: 
@@ -283,7 +287,7 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
+function addArtist(){
     /*Your Code Here*/
   }
 
